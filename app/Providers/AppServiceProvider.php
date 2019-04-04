d69488ad4632555361b3ae4cf4b5e6ad1123e38f
totti <724249769@qq.com>
2019-04-04 10:03:41 +0800

    201904041003

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // 添加自定义验证规则,允许字母、数字和 - _
+        Validator::extend('allow_letter', function ($attribute, $value, $parameters, $validator) {
+            return is_string($value) && preg_match('/^[A-Za-z0-9\-\_]+$/', $value);
+        });
+        Validator::replacer('allow_letter', function ($message, $attribute, $rule, $parameters) {
+            return str_replace(':attribute', '用户名', ':attribute 仅包含字母、数字.');
+        });
     }
 
     /**
