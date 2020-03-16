36beb81e6ecd0fddf4a6cca569b835cb5c7abe82
zgl <724249769@qq.com>
2020-03-16 22:46:08 +0800

    202003162246

--- app/Admin/Controllers/CategoryController.php
+++ app/Admin/Controllers/CategoryController.php
@@ -11,6 +11,7 @@ use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
 use Encore\Admin\Tree;
+use DB;
 
 class CategoryController extends Controller
 {
@@ -103,15 +104,15 @@ class CategoryController extends Controller
         return Admin::grid(Category::class, function (Grid $grid) {
 
             $grid->id('编号')->sortable();
-            $grid->column('cate_name','名称')->display(function ($cate_name){
-                return str_limit($cate_name,30);
+            $grid->column('cate_name', '名称')->display(function ($cate_name) {
+                return str_limit($cate_name, 30);
             });
-            $grid->parent_id('父类')->display(function ($parent_id){
-                $info=Category::find($parent_id);
+            $grid->parent_id('父类')->display(function ($parent_id) {
+                $info = Category::find($parent_id);
                 return $info->cate_name ?? 'ROOT';
             });
             //$grid->column('path','分类路径');
-            $grid->status('状态')->display(function ($status){
+            $grid->status('状态')->display(function ($status) {
                 return Category::statusMap()[$status] ?? '未知';
             });
 
@@ -136,38 +137,49 @@ class CategoryController extends Controller
     protected function form()
     {
         return Admin::form(Category::class, function (Form $form) {
-
+            $id = request()->route()->parameters()['category'] ?? 0;
             $form->display('id', '编号');
 
-            $id=request()->route()->parameters()['category'] ?? 0;
-
-            if($id){
-                $form->text('cate_name','名称')->rules('required|unique:category,cate_name,'.$id.',id');
-            }else{
-                $form->text('cate_name','名称')->rules('required|unique:category');
+            if ($id) {
+                $form->text('cate_name', '名称')->rules('required|unique:category,cate_name,' . $id . ',id');
+            } else {
+                $form->text('cate_name', '名称')->rules('required|unique:category');
             }
 
-            $form->select('parent_id','父类')->options(Category::selectOptions())->rules('required');
-
-            if($id){
-                $form->select('status','状态')->options(Category::statusMap());
+            $form->select('parent_id', '父类')->options(Category::selectOptions())->rules('required');
+            if ($id) {
+                $form->select('status', '状态')->options(Category::statusMap());
             }
 
             $form->display('created_at', '创建时间');
             //$form->display('updated_at', 'Updated At');
 
-            $form->saved(function (Form $form){
-                $info=Category::find($form->model()->id);
-                if($info->parent_id==0){
-                    $path=$form->model()->id;
-                }else{
-                    $parent=Category::find($info->parent_id);
-                    $path=$parent->cate_path.','.$form->model()->id;
-                }
-                $info->cate_path=$path;
-                $info->save();
-            });
+            $form->saved(function (Form $form) use ($id) {
+                if (empty($id)) {
+                    $info = Category::find($form->model()->id);
+                    if ($info->parent_id == 0) {
+                        $path = $form->model()->id;
+                    } else {
+                        $parent = Category::find($info->parent_id);
+                        $path = $parent->cate_path . ',' . $form->model()->id;
+                    }
 
+                    $info->cate_path = $path;
+                    $info->save();
+                } else {
+                    $info = Category::find($id);
+                    $old_path = $info['cate_path'];
+
+                    if ($info->parent_id == 0) {
+                        $new_path = $id . ',';
+                    } else {
+                        $parent = Category::find($info->parent_id);
+                        $new_path = $parent->cate_path . ',' . $id . ',';
+                    }
+
+                    DB::update("update category set cate_path=replace(cate_path,'{$old_path}','{$new_path}') where cate_path like '{$old_path}%'");
+                }
+            });
         });
     }
 }
